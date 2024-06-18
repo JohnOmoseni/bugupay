85ea0aab0315c295be2ef8b1d8a836ec9ec67236
John Omoseni <johnnyomoseni100@gmail.com>
2024-06-18 03:49:27 +0100

    chore: edited reveal animate component

--- src/components/RevealAnim.tsx
+++ src/components/RevealAnim.tsx
@@ -1,5 +1,5 @@
 import { motion, useInView, useAnimationControls } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 const revealVariants = {
